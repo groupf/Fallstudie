968ef0940926ee387e19013660550a2ec373239d
Micha Schönenberger <schoenm1@students.zhaw.ch>
2011-11-24 21:17:19 +0100

    gelöscht

--- chat_client/src/main/java/ch/hszt/groupf/fallstudie/client/socket/SocketClientConsumerIfc.java
+++
@@ -1,8 +0,0 @@
-package ch.hszt.groupf.fallstudie.client.socket;
-
-public interface SocketClientConsumerIfc {
-
-	public void onDisconnected(Exception inEx);
-	public void onReceivedMsg(String inMessage);
-	
-}
