4c2a24704b9ff42274de19db59bf40b507728c7c
esterren <renato.estermann@gmail.com>
2011-11-30 13:39:23 +0100

    added test testSocketServerStopsOnIOEx

--- chat_server/src/main/java/ch/hszt/groupf/fallstudie/server/socket/SocketServer.java
+++ chat_server/src/main/java/ch/hszt/groupf/fallstudie/server/socket/SocketServer.java
@@ -11,22 +11,56 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ch.hszt.groupf.fallstudie.server.srvconfig.ServerDefaultConfig;
+
+/**
+ * The SocketServer Class contains the most important Part of the Chat-Server.
+ * It listens for incoming Chat-Client connections, opens a new Socket due to
+ * the that request and starts a new ServerThread which corresponds to that new
+ * Chat-Client connection. The open connections are kept in a Map with the
+ * username as a Key and the DataOutputStream as the value from the
+ * user-corresponding Socket.
+ * 
+ * @author rest
+ * 
+ */
 public class SocketServer {
 
 	final static Logger logger = LoggerFactory.getLogger(SocketServer.class);
 	private ServerSocket _serverSocket;
 	private Map<String, DataOutputStream> _openOutputStreams = new Hashtable<String, DataOutputStream>();
 
+	/**
+	 * The Constructor starts the SocketServer.
+	 * 
+	 * @param inServerPort
+	 *            the port on which the java.net.ServerSocket starts to listen.
+	 */
 	public SocketServer(int inServerPort) {
 
 		listen(inServerPort);
 	}
 
+	/**
+	 * 
+	 * @param inServerPort
+	 */
 	private void listen(int inServerPort) {
 
 		try {
-			_serverSocket = new ServerSocket(inServerPort);
-		} catch (IOException e) {
+			_serverSocket = newServerSocket(inServerPort);
+
+		} catch (IllegalArgumentException e) {
+			logger.info("Tried to start the ServerSocket on a illegal portnumber! Server will be started on the default port "
+					+ ServerDefaultConfig.SERVERPORT);
+
+			try {
+				_serverSocket = newServerSocket(ServerDefaultConfig.SERVERPORT);
+			} catch (IOException e1) {
+				logger.error("Unable to start the Server on Port " + inServerPort + ". Server will be stopped!");
+				System.exit(0);
+			}
+		} catch (IOException e2) {
 
 			logger.error("Unable to start the Server on Port " + inServerPort + ". Server will be stopped!");
 			// e.printStackTrace();
@@ -77,6 +111,10 @@ public class SocketServer {
 
 			_openOutputStreams.put(inUserName, dos);
 		}
+	}
+
+	protected ServerSocket newServerSocket(int inServerPort) throws IOException {
+		return new ServerSocket(inServerPort);
 	}
 
 	protected void sendJoinedMsg(String inUserName) {
