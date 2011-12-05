4020a9d5d78004b623b5aace84805620b345e8ce
gruenean <gruenean@students.zhaw.ch>
2011-12-05 22:57:37 +0100

    chat client cli test class created

---
+++ chat_client/src/test/java/ch/hszt/groupf/fallstudie/client/cli/ChatClientCLITest.java
@@ -0,0 +1,14 @@
+package ch.hszt.groupf.fallstudie.client.cli;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ChatClientCLITest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
