26acb34408141252c4c51b9297f4edf3d30c51b7
esterren <renato.estermann@gmail.com>
2011-12-14 19:27:04 +0100

    fixed cli test quick

--- chat_client/src/test/java/ch/hszt/groupf/fallstudie/client/cli/ChatClientCLITest.java
+++ chat_client/src/test/java/ch/hszt/groupf/fallstudie/client/cli/ChatClientCLITest.java
@@ -1,20 +1,19 @@
 package ch.hszt.groupf.fallstudie.client.cli;
 
-import static org.junit.Assert.*;
-import ch.hszt.groupf.fallstudie.client.controller.*;
-
 import org.junit.Test;
+
+import ch.hszt.groupf.fallstudie.client.controller.ClientController;
 
 public class ChatClientCLITest {
 
 	private ClientController controller;
 	private ChatClientCLI cli;
-	
+
 	public void setUp() {
-		controller = new ClientController(true, true);
-	
+		controller = new ClientController(true);
+
 	}
-	
+
 	@Test
 	public void testConnect() {
 	}
