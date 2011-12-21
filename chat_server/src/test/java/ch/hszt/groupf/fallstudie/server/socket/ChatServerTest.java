6747b579bf0e21a05465917fdbc747840db68d81
esterren <renato.estermann@gmail.com>
2011-12-22 00:35:39 +0100

    tests ChatServer

--- chat_server/src/test/java/ch/hszt/groupf/fallstudie/server/socket/ChatServerTest.java
+++ chat_server/src/test/java/ch/hszt/groupf/fallstudie/server/socket/ChatServerTest.java
@@ -26,6 +26,7 @@ public class ChatServerTest {
 	private final Socket _socket = mock(Socket.class);
 	private final DataOutputStream _dos = mock(DataOutputStream.class);
 
+	private final static int maxClientConn = 10;
 	private int counter = 0;
 
 	@BeforeClass
@@ -81,29 +82,16 @@ public class ChatServerTest {
 	 * 
 	 * @throws IOException
 	 */
-	// TODO Testen, dass der SocketServer bei einer IOException stopt
+	// TODO Needs refactoring!!
 	@Test(expected = ExitException.class)
 	public void testChatServerStopsOnIOExInNewServerSocket() throws Exception {
 		final Object lock = new Object();
 		counter = 0;
-		// when(_chatServer.newServerSocket(ServerDefaultConfig.SERVERPORT)).then(new
-		// Answer<Object>() {
-		// public Object answer(InvocationOnMock invocation) throws Throwable {
-		// synchronized (lock) {
-		// try {
-		// counter++;
-		// throw new IOException();
-		// } finally {
-		// lock.notify();
-		// }
-		// }
-		// }
-		// });
+
 		_chatServer = new ChatServer(ServerDefaultConfig.SERVERPORT) {
 			@Override
 			protected ServerSocket newServerSocket(int inServerPort) throws IOException {
 
-				// throw new IOException();
 				synchronized (lock) {
 					try {
 						counter++;
@@ -112,7 +100,7 @@ public class ChatServerTest {
 						lock.notify();
 					}
 				}
-				// return _serverSocket;
+
 			}
 
 			@Override
@@ -134,31 +122,93 @@ public class ChatServerTest {
 				lock.wait();
 			}
 		}
-
-		// try {
-		// new ChatServer(ServerDefaultConfig.SERVERPORT) {
-		// @Override
-		// protected ServerSocket newServerSocket(int inServerPort) throws
-		// IOException {
-		// throw new IOException();
-		// }
-		//
-		// };
-		// } catch (ExitException e) {
-		// assertEquals("Exit status", 0, e.status);
-		// }
 		assertEquals(1, counter);
 	}
 
 	/**
-	 * Tests if the SocketServer calls system.exit(0), when the newServerSockets
-	 * throws an IOException
+	 * Tests if the amount of connections defined in maxClientConn are opened.
+	 * This means that the ChatServer only has to open this number of Sockets.
 	 * 
-	 * @throws IOException
+	 * 
 	 */
-	// TODO Testen, dass der SocketServer bei einer IOException stopt
 	@Test
-	public void testChatServerOneSingleConnect() throws Exception {
+	public void testChatServerClientConnections() throws Exception {
+		final Object lock = new Object();
+		counter = 0;
+		// when(_serverSocket.accept()).thenReturn(_socket);
+		when(_serverSocket.isClosed()).then(new Answer<Object>() {
+			public Object answer(InvocationOnMock invocation) {
+				synchronized (lock) {
+					try {
+						if (counter < maxClientConn) {
+							return false;
+						} else {
+							return true;
+						}
+
+					} finally {
+						lock.notify();
+					}
+				}
+			}
+		});
+
+		when(_serverSocket.accept()).then(new Answer<Object>() {
+			public Object answer(InvocationOnMock invocation) throws Throwable {
+				synchronized (lock) {
+					try {
+						if (counter < maxClientConn) {
+							counter++;
+							return _socket;
+						} else {
+							throw new IOException();
+						}
+
+					} finally {
+						lock.notify();
+					}
+				}
+			}
+		});
+
+		_chatServer = new ChatServer(ServerDefaultConfig.SERVERPORT) {
+			@Override
+			protected ServerSocket newServerSocket(int inServerPort) throws IOException {
+
+				return _serverSocket;
+			}
+
+			@Override
+			protected String getFirstIncomingString(Socket inSocket) throws IOException {
+
+				double rnd = Math.random() * maxClientConn * 10;
+				int rrnd = (int) rnd;
+				return "TestUser" + rrnd;
+
+			}
+
+			@Override
+			protected DataOutputStream getDosFromSocket(Socket inSocket) throws IOException {
+				return _dos;
+			}
+
+			@Override
+			protected void startNewServerThread(ChatServer inChatServer, Socket inSocket, String inSocketUserName) {
+
+			}
+		};
+		synchronized (lock) {
+			while (counter < maxClientConn) {
+				lock.wait();
+			}
+		}
+
+		assertEquals(maxClientConn, counter);
+	}
+
+	// TODO test sendJoindMsg storie
+	// @Test
+	public void testSendJoinedMsg() throws Exception {
 		final Object lock = new Object();
 		counter = 0;
 		// when(_serverSocket.accept()).thenReturn(_socket);
@@ -166,11 +216,9 @@ public class ChatServerTest {
 			public Object answer(InvocationOnMock invocation) {
 
 				synchronized (lock) {
-					// while (counter == 0) {
-					// lock.notify();
-					// }
+
 					try {
-						if (counter == 0) {
+						if (counter < maxClientConn) {
 							return false;
 						} else {
 							return true;
@@ -191,7 +239,7 @@ public class ChatServerTest {
 					// lock.notify();
 					// }
 					try {
-						if (counter == 0) {
+						if (counter < maxClientConn) {
 							counter++;
 							return _socket;
 						} else {
@@ -209,30 +257,13 @@ public class ChatServerTest {
 			@Override
 			protected ServerSocket newServerSocket(int inServerPort) throws IOException {
 
-				// throw new IOException();
-				// synchronized (lock) {
-				// try {
-				// counter++;
-				// throw new IOException();
-				// } finally {
-				// lock.notify();
-				// }
-				// }
 				return _serverSocket;
 			}
 
 			@Override
 			protected String getFirstIncomingString(Socket inSocket) throws IOException {
 
-				return "TestUser";
-				// synchronized (lock) {
-				// try {
-				// counter++;
-				// return "testuser";
-				// } finally {
-				// lock.notify();
-				// }
-				// }
+				return "TestUser1";
 
 			}
 
@@ -247,30 +278,12 @@ public class ChatServerTest {
 			}
 		};
 		synchronized (lock) {
-			while (counter == 0) {
+			while (counter < maxClientConn) {
 				lock.wait();
 			}
 		}
 
-		// try {
-		// new ChatServer(ServerDefaultConfig.SERVERPORT) {
-		// @Override
-		// protected ServerSocket newServerSocket(int inServerPort) throws
-		// IOException {
-		// throw new IOException();
-		// }
-		//
-		// };
-		// } catch (ExitException e) {
-		// assertEquals("Exit status", 0, e.status);
-		// }
-		assertEquals(1, counter);
-	}
-
-	// TODO test sendJoindMsg storie
-	// @Test
-	public void testSendJoinedMsg() {
-		// fail("Not yet implemented");
+		assertEquals(maxClientConn, counter);
 	}
 
 	// @Test
